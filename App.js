a1c0e1acddb6d584adc330a60c4b983ca11c53f9
Duy (Jay) Vu <duyvux5765@gmail.com>
2023-03-05 21:04:23 -0500

    Duy - change text color

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>RealXR</Text>
+      <Text style={styles.text}>RealXR</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    backgroundColor: "red",
   },
 });
