c075b4204dcfea22970a470e23ba9ba9c5b80b00
Duy (Jay) Vu <duyvux5765@gmail.com>
2023-03-05 22:04:48 -0500

    Duy - Test

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Notification from "./src/component/Notification.js";
+import { Panel, Form, FormGroup, FormControl, Button } from "react-bootstrap";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello- julian RealXR</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello- julian RealXR</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Notification />
+      <FormGroup controlId="formEmail">
+        <FormControl type="email" placeholder="Email Address" />
+      </FormGroup>
+    </>
   );
 }
 
