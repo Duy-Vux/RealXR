8327b40bf5e6e84477b76ae847c62fba4ab2a5be
Jason Rosales <rjasonr71022@gmail.com>
2023-04-13 11:48:08 -0400

    disover updates

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -11,10 +11,11 @@ import {
 import ConcertCard from "../component/ConcertCard";
 
 const data = [
-  { id: "1", title: "Trending", concerts: [1, 2, 3] },
-  { id: "2", title: "Live", concerts: [4, 5, 6] },
-  { id: "3", title: "Upcoming", concerts: [7, 8, 9] },
-  { id: "4", title: "Suggested Artist", concerts: [10, 11, 12] },
+  { id: "1", title: "Trending", concerts: [1, 2, 3], showBigImage: true },
+  { id: "2", title: "Live", concerts: [4, 5, 6], showBigImage: true },
+  { id: "3", title: "Upcoming", concerts: [7, 8, 9], showBigImage: true },
+  { id: "4", title: "Suggested Artist", concerts: [10, 11, 12], showBigImage: true },
+  { id: "5", title: "Featured Artist", concerts: [13, 14, 15], showBigImage: false },
 ];
 
 const ConcertRow = ({ concerts, navigation }) => (
@@ -32,32 +33,46 @@ const ConcertRow = ({ concerts, navigation }) => (
     ))}
   </ScrollView>
 );
+
 export default class Discover extends React.Component {
   render() {
     return (
       <>
-      <View style={styles.container}>
-        <FlatList 
-          data={data}
-          keyExtractor={(item) => item.id}
-          ListHeaderComponent={() => (
-            <View style={{ marginTop: 10, paddingTop: 10 }}>
-              <Text style={{ fontSize: 40, textAlign: "center", fontFamily: 'Cochin', fontWeight: 'bold'}}>
-                Discover Concerts
-              </Text>
-            </View>
-          )}
-          renderItem={({ item }) => (
-            <View style={{ marginTop: 40, backgroundColor: "#ffffff", paddingLeft: 10, paddingBottom: 10, paddingRight: 10}}>
-              <Text style={{ paddingLeft: 15, fontSize: 25, paddingTop: 10, fontFamily: 'Cochin', fontWeight: 'bold'}}>{item.title}</Text>
-              <ConcertRow
-                concerts={item.concerts}
-                navigation={this.props.navigation}
-              />
-            </View>
-          )}
-        />
-        <StatusBar style="auto" />
+        <View style={styles.container}>
+          <FlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            ListHeaderComponent={() => (
+              <View style={{ marginTop: 10, paddingTop: 10 }}>
+                <Text style={{ fontSize: 40, textAlign: "center", fontFamily: 'Cochin', fontWeight: 'bold'}}>
+                  Discover Concerts
+                </Text>
+              </View>
+            )}
+            renderItem={({ item }) => (
+              <View style={{ marginTop: 40, backgroundColor: "#ffffff", paddingLeft: 10, paddingBottom: 10, paddingRight: 10}}>
+                <Text style={{ paddingLeft: 15, fontSize: 25, paddingTop: 10, fontFamily: 'Cochin', fontWeight: 'bold'}}>{item.title}</Text>
+                {item.showBigImage ? (
+                  <ConcertRow concerts={item.concerts} navigation={this.props.navigation} />
+                ) : (
+                  <ScrollView horizontal style={{ paddingHorizontal: 10 }}>
+                    {item.concerts.map((concert) => (
+                      <TouchableOpacity
+                        key={concert}
+                        onPress={() => {
+                          navigation.navigate("ConcertDetails");
+                        }}
+                        style={{ marginRight: 25 }}
+                      >
+                        <View style={{ width: 80, height: 80, borderRadius: 40, backgroundColor: 'gray' }} />
+                      </TouchableOpacity>
+                    ))}
+                  </ScrollView>
+                )}
+              </View>
+            )}
+          />
+          <StatusBar style="auto" />
         </View>
       </>
     );
